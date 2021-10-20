4505152cbc2a44fac59b4154fe4b7ec4724687db
Iyonsi Israel <elyon4001@gmail.com>
2021-10-20 05:41:52 +0100

    portfolio new

---
+++ src/app/_Models/aboutme.ts
@@ -0,0 +1,12 @@
+export interface Aboutme {
+  firstName:string,
+  lastName:string,
+  about: string,
+  email: string,
+  socialMedia:string,
+  linkedIn:string,
+  github:string,
+  phone:string
+
+
+}
--- src/app/components/about-me/about-me.component.ts
+++ src/app/components/about-me/about-me.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Aboutme } from 'src/app/_Models/aboutme';
 import { PortfolioService1Service } from 'src/app/_services/portfolioService1.service';
 
 @Component({
@@ -8,9 +9,12 @@ import { PortfolioService1Service } from 'src/app/_services/portfolioService1.se
 })
 export class AboutMeComponent implements OnInit {
 
+  myBio :Aboutme[] = [];
+  
   constructor(private aboutMePortfolioService: PortfolioService1Service) { }
 
-  ngOnInit(): void { this.aboutMePortfolioService.getPortfolioData().subscribe((data :any)=>{console.log(data.data)})}
-  
+  ngOnInit(): void { this.aboutMePortfolioService.getPortfolioData().subscribe((data :any)=>{this.myBio=data.data.about})
+}
+
 
 }
